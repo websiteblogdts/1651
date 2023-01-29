f75cb8c415a047847f5391f1c68feeb60203f847
websiteblogdts <69079613+websiteblogdts@users.noreply.github.com>
2023-01-29 12:36:52 +0700

    update

---
+++ homework_2/homework_2/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_2
+{
+    class Rectangle : Object
+    {
+        public Point TopLeft { get; set; }
+        public Point BottomRight { get; set; }
+        public Rectangle()
+        {
+
+        }
+        public Rectangle(Point topLeft, Point bottomRight)
+        {
+            TopLeft = topLeft;
+            BottomRight = bottomRight;
+        }
+        public override string ToString()
+        {
+            return $"({TopLeft},{BottomRight})";
+        }
+        public bool IsContainsPoint(Point point)
+        {
+            bool IsCheckX = TopLeft.X <= point.X && point.X <= BottomRight.X;
+            bool IsCheckY = TopLeft.Y >= point.Y && point.Y >= BottomRight.Y;
+            return IsCheckX && IsCheckY;
+        }
+    }
+}
---
+++ homework_3/homework_3/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace homework_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Function function = new Function();
+            Console.WriteLine("Choose the function below");
+            function.Menu();
+            Console.Write("Enter your choice: ");
+            int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine("--------------------");
+            do
+            {
+                if (choice == 1)
+                {
+                    function.AddAStudentToList();
+                }
+                else if (choice == 2)
+                {
+                    Console.Write("Enter student name: ");
+                    string studentName = Console.ReadLine();
+                    Console.WriteLine("*****");
+                    function.FindByName(studentName);
+                }
+                else if (choice == 3)
+                {
+                    Console.WriteLine("End the program!");
+                    Console.WriteLine("*****");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice! Please try again!");
+                    Console.WriteLine("*****");
+                }
+                function.Menu();
+                Console.Write("Enter your choice: ");
+                choice = int.Parse(Console.ReadLine());
+            }
+            while (true);
+            Console.ReadLine();
+        }
+    }
+}
